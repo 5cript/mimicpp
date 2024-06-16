51a47eed1b9a0aac8310a64ad1431e9842772221
DNKpp <DNKpp@users.noreply.github.com>
2024-06-16 14:48:25 +0000

    deploy: c2251f84275db54393abf7fc96c9959778696dfd

--- index.js
+++ index.js
@@ -4,5 +4,11 @@ var index =
     [ "License", "index.html#autotoc_md8", null ],
     [ "Introduction", "index.html#autotoc_md10", [
       [ "Special Acknowledgement", "index.html#autotoc_md11", null ]
+    ] ],
+    [ "Documentation", "index.html#autotoc_md12", null ],
+    [ "Testing", "index.html#autotoc_md13", [
+      [ "Windows", "index.html#autotoc_md14", null ],
+      [ "Linux", "index.html#autotoc_md15", null ],
+      [ "MacOs", "index.html#autotoc_md16", null ]
     ] ]
 ];
