264b64f20ddff3113b5cfd01c14556f0c1230078
DNKpp <DNKpp@users.noreply.github.com>
2024-10-06 00:32:21 +0000

    deploy: 20c4c6b0f167c7b3ac78073e7f25eead0c652e7c

--- search/all_16.js
+++ search/all_16.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['where_0',['where',['../db/d10/classmimicpp_1_1_error.html#ac580333daeda5cf750c58c5b3189c427',1,'mimicpp::Error']]],
-  ['why_20is_20it_20an_20experimental_20feature_1',['Why is it an experimental feature',['../d2/d65/group___c_o_n_f_i_g_u_r_a_t_i_o_n.html#autotoc_md20',1,'Why is it an experimental feature?'],['../d2/d65/group___c_o_n_f_i_g_u_r_a_t_i_o_n.html#autotoc_md24',1,'Why is it an experimental feature?']]],
+  ['why_20is_20it_20an_20experimental_20feature_1',['Why is it an experimental feature',['../d2/d65/group___c_o_n_f_i_g_u_r_a_t_i_o_n.html#autotoc_md21',1,'Why is it an experimental feature?'],['../d2/d65/group___c_o_n_f_i_g_u_r_a_t_i_o_n.html#autotoc_md24',1,'Why is it an experimental feature?']]],
   ['wildcardmatcher_2',['WildcardMatcher',['../db/de6/classmimicpp_1_1_wildcard_matcher.html',1,'mimicpp']]],
   ['windows_3',['Windows',['../index.html#autotoc_md54',1,'']]],
   ['with_20other_20character_20types_4',['Strings with other character-types',['../de/dd6/group___e_x_p_e_c_t_a_t_i_o_n___m_a_t_c_h_e_r_s___s_t_r_i_n_g.html#autotoc_md9',1,'']]],
